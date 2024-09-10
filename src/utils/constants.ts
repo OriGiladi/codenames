3248a0eb8116a56ccaa0c5dd90cc7906e7b43b69
origiladi <120328585+origiladi@users.noreply.github.com>
2024-09-10 16:36:40 +0300

    added env file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
+export const REST_API_BASE_URL = import.meta.env.VITE_REST_API_BASE_URL 
+
 export const userRoles = {
     CODE_MASTER: "code-master",
     PLAYER: "player"
 } 
-
-export const REST_API_BASE_URL = 'http://localhost:3001'
